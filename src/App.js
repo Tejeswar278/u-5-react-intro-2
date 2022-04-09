4722081aa28903ea43df30d79760bfde9ef0b664
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-04-10 01:34:27 +0530

    partially completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const components = ["LOGOBAKERY", ""]
   return (
-    <div className="App">
-      
+    <div className="App background">
+      <div className='paddingT'>
+      <nav className='dimensions'>
+        <div>
+
+        </div>
+      </nav>
+      </div>
     </div>
   );
+}
+function compo(){
+  return <div></div>
 }
 
 export default App;
